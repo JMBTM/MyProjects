8032da4a824766946882db1feae4adbaba7bbd90
JMBTM <tolentinojohnmichael97@gmail.com>
2015-11-21 06:03:26 +0800

    Demo new File in PHP

--- zoomJquery/demo.php
+++ zoomJquery/demo.php
@@ -4,65 +4,55 @@
 	<title>Demo</title>
 	<link href="http://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
 	<link rel="stylesheet" type="text/css" href="t/stylesheets/app.css">
-	<script type="text/javascript" src="jquery2.1.1.js"></script>
-	<script type="text/javascript">
-	$(document).ready(function(){
-		var container = $('#container');
-		var lmargin = container.offset().left;
-		var tmargin = container.offset().top;
-
-		/*$('#container').mousemove(function(E){
-			var xcenter = E.pageX - lmargin;
-			var ycenter = E.pageY - tmargin;
-
-			var l = xcenter - 100;
-			var t = ycenter - 100;
-			$('#magnifier').css({left: l + 'px', top: t + 'px'});
-
-			var limg = -(xcenter * 1) + 500;
-			var timg = -(ycenter * 1) + 500;
-
-			$('#magnifier').css({backgroundPosition: limg+'px ' + timg+'px'});
-		})*/
-	});
-
-	</script>
 	<style>
-	#container{
-		width:512px;
-		height:384px;
-		margin:100px auto;
-		overflow:hidden;
+	#ip-viewfinder{
+		border:dotted #ccc 1px;
+		background:rgba(255,255,255,0.5);
+		clear:both;
+	}
+	.wrap{
+		float:left;
+		display:block;
 		position:relative;
-		background-color:blue;
-		border-radius:5px;
 	}
-	img.thumbnail{
-		width:512px;
-		height:384px;display:none;
-	}
-	img.thumbnail:hover{
-		cursor:crosshair;
-	}
-	#magnifier{
-		width:200px;
-		height:200px;background-color:blue;
-		overflow:hidden;
-		background-image:url('../img/logo.png');
-		background-repeat:no-repeat;
-		background-size:1005px 1005px;
-		background-position:-100px 1px;
+	.overlay{
+		width:100%;
+		height: 100%;
+		display:block;
 		position:absolute;
-		left:0px;
-		cursor:crosshair;
-		border:2px solid #fff;
+		z-index:2;
+	}
+	.overlay:after{
+		content:"Hover me!";
+		position:absolute;
+		left:50%;
+		top:50%;
+		margin-left:-80px;
+		margin-top:-1.5em;
+		width:160px;
+		height:3em;
+		line-height:3em;
+		text-align:center;
+		background:rgba(255,255,255,0.5);
+		font-weight: bold;
 	}
 	</style>
 </head>
 <body>
-	<div id="container">
-		<img class="thumbnail" src="../img/logo.png"/>
-		<div id="magnifier"></div>
-	</div>
+	<div class="container" style="margin-top:150px;">
+  <h1>jQuery Image Projection Plugin Demo</h1>
+  <div class="wrap">
+    <div class="overlay"></div>
+  <img src="../img/logo.png" id="sampleimg" data-pimg="../img/logo.png"/>
+</div>
+</div>
+	<script type="text/javascript" src="jquery2.1.1.js"></script>
+	<script type="text/javascript" src="t/javascripts/image.projection.js"></script>
+	<script type="text/javascript" src="t/javascripts/app.js"></script>
+	<script type="text/javascript">
+	$(window).load(function(){
+		$(".wrap .overlay").imageProjection($("#sampleimg"));
+	});
+	</script>
 </body>
 </html>
